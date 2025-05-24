82ae0ae5811dd0b8d8fd47a2776fa72120cf821e
GerardAsuncion <asunciongerard90@gmail.com>
2025-05-24 18:29:20 +0200

    header margins changed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@
 const Header = () => {
   return (
     <header className="bg-linear-to-r/srgb from-indigo-500 to-teal-400 w-90/100 h-20 rounded-lg flex justify-center
-    items-center">
+    items-center m-auto mt-5">
         <h1 className="text-xl font-bold">Aconsegueix la millor qualitat</h1>
     </header>
   )
