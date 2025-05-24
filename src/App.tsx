3ec332dd37cca1b55b3e43ac80a446c412aada03
GerardAsuncion <asunciongerard90@gmail.com>
2025-05-24 19:37:03 +0200

    Checkbox component map

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import sections from "./data/sections.json"
 import Header from "./components/Header"
+import Checkbox from "./components/Checkbox"
 
 function App() {
  
   return (
     <>
       <Header></Header>
+      {sections.map(section => <Checkbox key={section.id} currentSection={section}></Checkbox>)}
     </>
   )
 }
---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,31 @@
+type Section = {
+    id: number,
+    section: string,
+    price: number,
+    isWeb: boolean
+}
+
+type Props = {
+    currentSection: Section
+}
+
+const Checkbox = ({currentSection}: Props) => {
+  return (
+    <section className="w-90/100 h-20 rounded-lg flex justify-center
+    items-center m-auto mt-5">
+        <article>
+            <h2>{currentSection.section}</h2>
+            <p>Programació d'una web responsive completa</p>
+        </article>
+        <article>
+            {currentSection.price}€
+        </article>
+        <article>
+            <input type="checkbox" name="add" id="add" />
+            <label htmlFor="add">Afegir</label>
+        </article>
+    </section>
+  )
+}
+
+export default Checkbox
