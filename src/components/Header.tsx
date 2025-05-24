475a2c1bcefe789b0b3b124818eeb701f990aa3d
GerardAsuncion <asunciongerard90@gmail.com>
2025-05-24 18:10:45 +0200

    header created

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+  return (
+    <header className="bg-linear-to-r/srgb from-indigo-500 to-teal-400 w-90/100 h-20 rounded-lg flex justify-center
+    items-center">
+        <h1 className="text-xl font-bold">Aconsegueix la millor qualitat</h1>
+    </header>
+  )
+}
+
+export default Header
