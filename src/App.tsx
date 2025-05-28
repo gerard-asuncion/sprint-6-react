e7b795ae3902b8e1b7bb206eabd21974c9a6cf46
GerardAsuncion <asunciongerard90@gmail.com>
2025-05-28 13:32:20 +0200

    fix: main and App updated to work with react-router-dom, ex 4 complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,15 @@
-import Header from "./components/Header"
-import Checkbox from "./components/Checkbox"
-import Budget from "./components/Budget"
-import PriceProvider from "./context/PriceProvider"
-import SectionsProvider from "./context/SectionsProvider";
-import useIds from "./hooks/useIds";
+import { Routes, Route } from "react-router-dom"
+import MainApp from "./components/MainApp";
+import Welcome from "./components/Welcome";
 
 function App() {
 
-  const { arrSectionsId } = useIds()
-
   return (
-    <SectionsProvider>
-      <PriceProvider>
-        <Header></Header>
-        {arrSectionsId.map(id => 
-        <Checkbox 
-        key={id} 
-        id={id}
-        ></Checkbox>)}
-        <Budget></Budget>
-      </PriceProvider>
-    </SectionsProvider>
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+      <Route path="/app" element={<MainApp />} />
+    </Routes>
   )
 }
 
 export default App
-
-// {sections.map(section => 
-//         <Checkbox 
-//         key={section.id} 
-//         currentSection={section}
-//         active={section.isChecked}
-//         onToggle={(e) => {
-//           handleChange(e.target.checked, section.id);
-//           capturateCheck(e.target.checked);
-//         }}
