51cdd60e2d2f12ef65a3024c9d4694e98f35f046
GerardAsuncion <asunciongerard90@gmail.com>
2025-05-29 13:01:20 +0200

    feat: MainApp now includes BudgetForm

--- src/components/MainApp.tsx
+++ src/components/MainApp.tsx
@@ -1,28 +1,31 @@
 import Header from "./Header"
 import Checkbox from "./Checkbox"
-import Budget from "./Budget"
+import TotalBudget from "./TotalBudget"
 import BudgetForm from "./BudgetForm"
 import PriceProvider from "../context/PriceProvider"
 import SectionsProvider from "../context/SectionsProvider";
 import useIds from "../hooks/useIds";
+import BudgetsProvider from "../context/BudgetsProvider"
 
 const MainApp = () => {
 
   const { arrSectionsId } = useIds()
 
   return (
-    <SectionsProvider>
-      <PriceProvider>
-        <Header></Header>
-        {arrSectionsId.map(id => 
-        <Checkbox 
-        key={id} 
-        id={id}
-        ></Checkbox>)}
-        <Budget></Budget>
-        <BudgetForm></BudgetForm>
-      </PriceProvider>
-    </SectionsProvider>
+    <BudgetsProvider>
+      <SectionsProvider>
+        <PriceProvider>
+          <Header></Header>
+          {arrSectionsId.map(id => 
+          <Checkbox 
+          key={id} 
+          id={id}
+          ></Checkbox>)}
+          <TotalBudget></TotalBudget>
+          <BudgetForm></BudgetForm>
+        </PriceProvider>
+      </SectionsProvider>
+    </BudgetsProvider>
   )
 }
 
