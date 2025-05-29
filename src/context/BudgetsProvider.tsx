d6ab0b3bb8d1a28eb0518bb7c805e6e25a046d40
GerardAsuncion <asunciongerard90@gmail.com>
2025-05-29 13:10:03 +0200

    feat: created a context that recovers the array of budgets stored in local storage and makes it possible to use it in multiple hooks

---
+++ src/context/BudgetsProvider.tsx
@@ -0,0 +1,27 @@
+import { BudgetsContext } from "./BudgetsContext"
+import type { ProviderProps } from "../types/types"
+import { useEffect, useState } from "react"
+import { Budget } from "../classes/Budget"
+
+const BudgetsProvider = ({children}: ProviderProps) => {
+
+    const [budgets, setBudgets] = useState<Budget[]>(() => {
+      const saved = localStorage.getItem('arrBudgets');
+      if (!saved) return [];
+
+      const parsed = JSON.parse(saved);
+      return parsed.map((obj: any) => Budget.fromJSON(obj));
+    })
+
+    useEffect(() => {
+      localStorage.setItem('arrBudgets', JSON.stringify(budgets.map(budget => budget.toJSON())));
+    }, [budgets])
+
+  return (
+    <BudgetsContext.Provider value={{budgets, setBudgets}}>
+        {children}
+    </BudgetsContext.Provider>
+  )
+}
+
+export default BudgetsProvider
