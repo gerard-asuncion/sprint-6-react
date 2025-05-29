01774fecce54f06004517f11ca6c48edaece0227
GerardAsuncion <asunciongerard90@gmail.com>
2025-05-29 12:55:54 +0200

    feat: useSubmit is now operative to add budgets to an array stored in local storage, as well as deleting this array

--- src/hooks/useSubmit.ts
+++ src/hooks/useSubmit.ts
@@ -1,19 +1,35 @@
 import { useState, useContext } from "react"
 import { PriceContext } from "../context/PriceContext"
+import { Budget } from "../classes/Budget"
+import { BudgetsContext } from "../context/BudgetsContext"
 
 const useSubmit = () => {
 
+    const { budgets, setBudgets } = useContext(BudgetsContext)
     const { totalPrice } = useContext(PriceContext)
 
-    const [name, setName] = useState("")
-    const [phone, setPhone] = useState(0)
-    const [email, setEmail] = useState("")
+    const [name, setName] = useState<string>("")
+    const [phone, setPhone] = useState<number>(0)
+    const [email, setEmail] = useState<string>("")
 
-    const handleSubmit = () => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+
+        e.preventDefault()
+
+        const newBudget: Budget = new Budget(name, phone, email, totalPrice)
+
+        setBudgets(prevArr => [...prevArr, newBudget])    
 
     }
 
-    return { setName, setPhone, setEmail, handleSubmit }
+    const resetBudgets = () => {
+
+        setBudgets([])
+        localStorage.removeItem("arrBudgets")
+
+    }
+
+    return { setName, setPhone, setEmail, handleSubmit, resetBudgets }
   
 }
 
