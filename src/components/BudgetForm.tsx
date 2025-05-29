aa65a6b9b70bc496e9b007d311b7de20ec952ffd
GerardAsuncion <asunciongerard90@gmail.com>
2025-05-29 12:57:58 +0200

    feat: BudgetForm is now fully funcitonal but it still lacks validation

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,38 +1,51 @@
 import useSubmit from "../hooks/useSubmit"
+import { useContext } from "react"
+import { BudgetsContext } from "../context/BudgetsContext"
 
 const BudgetForm = () => {
 
-  const { setName, setPhone, setEmail, handleSubmit } = useSubmit()
+  const { budgets } = useContext(BudgetsContext)
+
+  const { setName, setPhone, setEmail, handleSubmit, resetBudgets } = useSubmit()
+
+  console.log(budgets)
 
   return (
     <section className="flex flex-col items-around w-90/100 p-4 rounded-lg m-auto shadow-md">
       <h2 className="text-lg font-bold">Demanar pressupost</h2>
       <form onSubmit={handleSubmit}>
         <input
-            className="border-2 border-gray-500 rounded-md"
-            type="text" 
-            name="name" 
-            id="name"
-            onChange={(e) => setName(e.target.value)}
-            placeholder="Nom" />
+          className="border-2 border-gray-500 rounded-md"
+          type="text" 
+          name="name" 
+          id="name"
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Nom" />
         <input 
-            className="border-2 border-gray-500 rounded-md"  
-            type="number" 
-            name="phone" 
-            id="phone"
-            onChange={(e) => setPhone(Number(e.target.value))}
-            placeholder="Telèfon" />
+          className="border-2 border-gray-500 rounded-md"  
+          type="number" 
+          name="phone" 
+          id="phone"
+          onChange={(e) => setPhone(Number(e.target.value))}
+          placeholder="Telèfon" />
         <input 
-            className="border-2 border-gray-500 rounded-md"
-            type="email" 
-            name="email" 
-            id="email"
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email" />
-        <button type="submit">
+          className="border-2 border-gray-500 rounded-md"
+          type="email" 
+          name="email" 
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email" />
+        <button 
+          className="text-white bg-gray-800 border-2 border-gray-800 rounded-md"
+          type="submit">
           Sol·licitar pressupost
         </button>
       </form>
+      <button 
+          className="text-white bg-gray-800 border-2 border-gray-800 rounded-md"
+          onClick={resetBudgets}>
+          Reset
+        </button>
     </section>
   )
 }
