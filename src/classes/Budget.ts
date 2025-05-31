b20f2804e48315a2f835b805d2340daf701d29f8
GerardAsuncion <asunciongerard90@gmail.com>
2025-05-31 15:52:20 +0200

    fix: class Budget properties are now private, property budget changed to totalBudget because the entire object is already called budget

--- src/classes/Budget.ts
+++ src/classes/Budget.ts
@@ -1,14 +1,14 @@
 export class Budget {
 
-    public name: string;
-    public phone: number;
-    public email: string;
-    public budget: number;
-    public date: string;
-    public id: number;
+    #name: string;
+    #phone: number;
+    #email: string;
+    #totalBudget: number;
+    #date: string;
+    #id: number;
 
     static fromJSON(obj: any): Budget {
-        return new Budget(obj.name, obj.phone, obj.email, obj.budget);
+        return new Budget(obj.name, obj.phone, obj.email, obj.totalBudget);
     }
 
     static date: string = new Date().toISOString();
@@ -16,21 +16,28 @@ export class Budget {
     static counter = 0
 
     constructor(name: string, phone: number, email: string, totalBudget: number){
-        this.name = name,
-        this.phone = phone,
-        this.email = email,
-        this.budget = totalBudget,
-        this.date = Budget.date
+        this.#name = name,
+        this.#phone = phone,
+        this.#email = email,
+        this.#totalBudget = totalBudget,
+        this.#date = Budget.date
         Budget.counter++
-        this.id = Budget.counter
+        this.#id = Budget.counter
     }
+
+    get id() { return this.#id }
+    get name() { return this.#name }
+    get phone() { return this.#phone }
+    get email() { return this.#email }
+    get totalBudget() { return this.#totalBudget }
+    get date() { return this.#date }
 
     toJSON() {
         return {
             name: this.name,
             phone: this.phone,
             email: this.email,
-            budget: this.budget,
+            totalBudget: this.#totalBudget,
             date: this.date,
             id: this.id
         }
