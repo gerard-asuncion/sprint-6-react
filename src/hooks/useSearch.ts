7edf428610d35d4943244c69cb5c29ba9c1975a7
GerardAsuncion <asunciongerard90@gmail.com>
2025-06-03 17:55:08 +0200

    fix: simplified instantSearch function

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -8,26 +8,20 @@ const useSearch = () => {
 
   const [search, setSearch] = useState("")
 
-  const nameSearch = (search: string): Budget[] => budgets.filter(budget => budget.name.toLowerCase().includes(search.toLowerCase()))
-  const emailSearch = (search: string): Budget[] => budgets.filter(budget => budget.email.toLowerCase().includes(search.toLowerCase()))
-  const phoneSearch = (search: string): Budget [] => budgets.filter(budget => budget.phone.includes(search))
-
   const instantSearch = (search: string): Budget[] => {
   
-    const foundNames: Budget[] = nameSearch(search)
-    const foundEmails: Budget[] = emailSearch(search)
-    const foundPhones: Budget[] = phoneSearch(search)
+    const foundNames: Budget[] = budgets.filter(budget => budget.name.toLowerCase().includes(search.toLowerCase()))
+    const foundEmails: Budget[] = budgets.filter(budget => budget.email.toLowerCase().includes(search.toLowerCase()))
+    const foundPhones: Budget[] = budgets.filter(budget => budget.phone.includes(search))
 
-    const merged: Budget[] = [...foundNames, ...foundEmails, ...foundPhones]
-    const foundBudgets: Budget[] = [...new Set(merged)]
-    const sortedBudgets: Budget[] = foundBudgets.sort((a, b) => 
+    const foundBudgets: Budget[] = [...new Set([...foundNames, ...foundEmails, ...foundPhones])].sort((a, b) => 
       a.name === b.name
       ? a.totalBudget === b.totalBudget
         ? a.date.getTime() - b.date.getTime()
         : a.totalBudget - b.totalBudget
       : a.name.localeCompare(b.name))
 
-    return sortedBudgets
+    return foundBudgets
 
   }
 
