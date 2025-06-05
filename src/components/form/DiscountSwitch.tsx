045c9c65196a196f2150324487c88134f9fbdaad
GerardAsuncion <asunciongerard90@gmail.com>
2025-06-05 12:57:18 +0200

    feat: component and hook for discount management created

---
+++ src/components/form/DiscountSwitch.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react"
+
+const DiscountSwitch = () => {
+
+  const [discount, setDiscount] = useState<boolean>()
+
+  useEffect(() => {   
+        
+    }, [discount])
+
+  return (
+    <article className="flex justify-center w-90/100 p-4 m-auto mt-5 gap-7">
+        <div className="text-neutral-600">Pagament mensual</div>
+        <label className="relative inline-block w-12 h-6">
+            <input 
+                type="checkbox" 
+                className="peer sr-only"
+                name="discountSwitch" 
+                checked={discount} 
+                onChange={(e) => {setDiscount(e.target.checked)}} />
+            <div className="w-full h-full bg-gray-300 rounded-full peer-checked:bg-green-500 transition-colors duration-300"></div>
+            <div className="absolute top-0.5 left-0.5 w-5 h-5 bg-white rounded-full transition-transform duration-300 peer-checked:translate-x-6"></div>
+        </label>
+        <div className="text-neutral-600">Pagament anual</div>
+    </article>
+  )
+}
+
+export default DiscountSwitch
---
+++ src/hooks/useDiscount.tsx
@@ -0,0 +1,9 @@
+import { useContext } from "react"
+import { SectionsContext } from "../context/sections/SectionsContext"
+
+const useDiscount = () => {
+    
+    
+}
+
+export default useDiscount
