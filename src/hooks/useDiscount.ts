759a12a4dd77dfffdb447758827ae849414148c6
GerardAsuncion <asunciongerard90@gmail.com>
2025-06-11 12:31:51 +0200

    feat: useDiscount hook created

--- src/hooks/useDiscount.ts
+++ src/hooks/useDiscount.ts
@@ -1,10 +1,20 @@
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { DiscountContext } from "../context/discount/DiscountContext"
+import { SectionsContext } from "../context/SectionsContext"
+import { applyDiscount } from "../utils/utils"
 
 
 const useDiscount = () => {
 
-    const { discount, setDiscount } = useContext(DiscountContext)
+    const { discount } = useContext(DiscountContext)
+    const { setSections } = useContext(SectionsContext)
+
+    useEffect(() => {
+
+        setSections(prev => prev.map(section => { return {...section, hasDiscount: !section.hasDiscount} }))
+        setSections(prev => prev.map(section => { return {...section, price: applyDiscount(section.price, section.hasDiscount)}}))
+
+    }, [discount])
  
 }
 
